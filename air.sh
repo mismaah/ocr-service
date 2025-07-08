5f7dcbcc085c1f670566da4d0ac0d8d7e0d1cf37
mismaah <mismaahabdulla@gmail.com>
2025-07-08 15:59:34 +0500

    ci: update air script

--- air.sh
+++ air.sh
@@ -1,5 +1,6 @@
+docker build . -t ocr-service
 docker stop ocr-service
 docker rm ocr-service
-docker build . -t ocr-service
 source .env
-docker run -p ${PORT}:${PORT} -e PORT=${PORT} -e AUTH_KEY=${AUTH_KEY} --name ocr-service ocr-service
+docker run -d -p ${PORT}:${PORT} -e PORT=${PORT} -e AUTH_KEY=${AUTH_KEY} --name ocr-service ocr-service
+docker logs -f --tail 100 ocr-service
